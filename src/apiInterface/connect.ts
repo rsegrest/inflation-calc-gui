f1f536245b4835256cfc59884855224c60bc63dc
Rick Segrest <rsegrest77@gmail.com>
2026-02-10 20:58:33 -0600

    Updated .env variable scheme

--- src/apiInterface/connect.ts
+++ src/apiInterface/connect.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const PROTOCOL = import.meta.env.VITE_USE_HTTPS ? "https" : "http";
 const PORT = import.meta.env.VITE_PORT || 5000;
-const API_URL = `${PROTOCOL}://${import.meta.env.VITE_IP_ADDRESS}:${PORT}` || `${PROTOCOL}://localhost:${PORT}`;
+const API_URL = import.meta.env.VITE_API_URL || `${PROTOCOL}://${import.meta.env.VITE_IP_ADDRESS}:${PORT}` || `${PROTOCOL}://localhost:${PORT}`;
 
 
 export const getRequestToApi = async ({ startingAmount, fromYear, toYear, fromMonth, toMonth }: { startingAmount: number, fromYear: number, toYear: number, fromMonth: number, toMonth: number }) => {
